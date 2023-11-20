47c59d015c571517b0b015dbc4c6303a63357f5f
PGPGP <101028772+PGPGP@users.noreply.github.com>
2023-11-20 15:56:52 +0800

    界面美化完成

--- web/src/views/recognition/recognition.vue
+++ web/src/views/recognition/recognition.vue
@@ -4,35 +4,51 @@
       <h1 class="h1" :style="styleOfH1">
         识别手写数字
       </h1>
-    <div class="image-preview-container">
-      <div >
-          <img :src="getImageUrl(this.selectedImage)" alt="待识别图片" class="preview-image">
+      <div class="image-preview-container">
+        <div class="preview-wrapper" :class="{ 'empty-preview': !selectedImage }">
+          <img v-if="selectedImage" :src="getImageUrl(selectedImage)" alt="待识别图片" class="preview-image">
+          <div v-else class="empty-preview-text">待识别图片</div>
         </div>
-    </div>
-      <div class="file-input-container">
+      </div>
+      <br>
+      <div class="button-container">
         <el-upload
           class="upload-demo"
           :before-upload="handleBeforeUpload"
           :show-file-list="false"
         >
-          <el-button size="small" type="primary" >选择文件</el-button>
+          <div class="button-container">
+            <el-button size="middle" type="primary">选择文件</el-button>
+          </div>
         </el-upload>
       </div>
-      <el-button @click="recognize" size="small" type="primary" >识别</el-button>
+      <br>
+      <div class="button-container">
+        <el-button @click="recognize" size="middle" type="primary">识别</el-button>
+      </div>
     </div>
 
     <div>
-      <h1  class="h1" :style="styleOfH1">
+      <h1 class="h1" :style="styleOfH1">
         识别结果
       </h1>
-      <div class="image-preview-container">
-      <div >
-          <img :src="base64ToUrl()" alt="识别结果图片" class="preview-image">
-        </div>
-    </div>
       <div class="result-container">
-        <div class="text-preview">
-          {{ this.recognizedText }}
+        <div class="image-preview-container">
+          <div class="preview-wrapper" :class="{ 'empty-preview': !this.recognizedImage }">
+            <img v-if="this.recognizedImage" :src="base64ToUrl()" alt="识别结果图片" class="preview-image">
+            <div v-else class="empty-preview-text">识别结果图片</div>
+          </div>
+        </div>
+        <table> </table>
+        <table> </table>
+        <table> </table>
+        <div class="text-preview-container">
+          <div>
+            识别结果文本：
+          </div>
+          <div class="text-preview">
+            {{ recognizedText }}
+          </div>
         </div>
       </div>
     </div>
@@ -54,12 +70,14 @@ export default {
       selectedImageUrl: '',
       recognizedImage: '',
       recognizedText: ''
-
     }
   },
   methods: {
-    // 在上传之前的钩子，用于保存文件
     handleBeforeUpload(file) {
+      if (!file.type.startsWith('image/')) {
+        this.$message.error('请选择图片文件！')
+        return false
+      }
       const reader = new FileReader()
       reader.onload = (e) => {
         this.selectedImageUrl = e.target.result
@@ -67,17 +85,10 @@ export default {
       }
       reader.readAsDataURL(file)
       this.selectedImage = file
-      return true // 允许上传
+      return true
     },
-    // 上传成功的处理方法
-    // handleSuccess(response) {
-    //   this.recognizedImage = response.imageUrl;
-    //   this.recognizedText = response.text;
-    // },
-    // 点击识别按钮时触发上传
     recognize() {
       if (!this.selectedImage) {
-        // 用户未选择图片，可以在这里添加逻辑以通知用户或执行其他操作
         this.$message('请先选择图片!')
         return
       }
@@ -85,13 +96,11 @@ export default {
       const formData = new FormData()
       formData.append('file', this.selectedImage)
       formData.append('postfix', this.selectedImagePostfix)
-      // 发送 POST 请求
       axios.post('http://localhost:8080/use/image', formData, {
         headers: {
-          'Content-Type': 'multipart/form-data' // 设置请求头
+          'Content-Type': 'multipart/form-data'
         }
       }).then(response => {
-        console.log('收到！')
         const { file, text } = response.data
         this.recognizedImage = file
         this.recognizedText = text
@@ -99,18 +108,14 @@ export default {
         console.error(error)
       })
     },
-
     getImageUrl(img) {
       if (img && img instanceof Blob) {
         return URL.createObjectURL(img)
       } else {
-        // 如果 image 不是 Blob 对象，可能需要根据实际情况返回其他 URL 或空字符串
         return ''
       }
     },
-
     base64ToUrl() {
-      // 将Base64字符串拼接成图片的Data URL
       return `data:image/jpg;base64,${this.recognizedImage}`
     }
   }
@@ -120,26 +125,52 @@ export default {
 <style>
   .result-container {
     display: flex;
+    align-items: center; /* 使内容垂直居中 */
+    justify-content: center; /* 使内容水平居中 */
   }
 
+  .text-preview-container {
+    height: 320px;
+    width: 500px;
+    border: 2px solid black; /* 为 recognizedText 添加黑色边框 */
+    padding: 10px;
+    margin-right: 10px; /* 可调整 recognizedText 和图片之间的间距 */
+  }
+
+  .text-preview {
+    overflow: auto;
+    font-size: 16px;
+    color: black;
+  }
+  .button-container {
+    text-align: center;
+  }
   .image-preview-container {
-      text-align: center;
+    text-align: center;
+  }
+  .preview-wrapper {
+    max-width: 100%;
+    max-height: 320px;
+    height: 320px;
+    width: 500px;
+    margin: 10px 0;
+    text-align: center;
+    border: 2px solid black; /* 设置黑边框 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    margin: auto; /* 水平居中 */
+  }
+  .empty-preview-text {
+    opacity: 0.5;
+    font-size: 16px;
+    color: black;
   }
   .preview-image {
     max-width: 100%;
     max-height: 300px;
     margin: 20px 0;
     border: 1px solid #ccc;
-  }
-
-  .text-preview {
-    flex: 1;
-    padding: 0 10px;
-    overflow: auto;
-  }
-
-  .file-input-container {
-    position: relative;
-    display: inline-block;
   }
 </style>
