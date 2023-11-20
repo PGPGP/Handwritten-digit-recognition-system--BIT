9f7de1a00ccfc38d14e21b273bed4fbb64514c7e
PGPGP <101028772+PGPGP@users.noreply.github.com>
2023-11-20 15:56:22 +0800

    数据统计功能完成

--- web/src/views/statistics/statistics.vue
+++ web/src/views/statistics/statistics.vue
@@ -1,257 +1,213 @@
 <template>
   <div class="app-container">
     <div class="h1" :style="styleOfH1">
-      <h1>
-        系统使用数据统计
-      </h1>
+      <h1>系统使用数据统计</h1>
     </div>
-    <br>
+    <br />
     <div class="choose" :style="styleOfChoose">
       系统使用模型
-      <el-select v-model="value" placeholder="请选择">
-        <el-option-group
-          v-for="group in options"
-          :key="group.label"
-          :label="group.label">
-          <el-option
-            v-for="item in group.options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-          </el-option>
-        </el-option-group>
-      </el-select>
-    </div>
-    <br><br>
-    <div style="margin-left: 10%;">
-      <el-radio-group v-model="radio">
-        <el-radio :label="1">年度</el-radio>
-        <el-radio :label="2">月度</el-radio>
-        <el-radio :label="3">周次</el-radio>
-        <el-radio :label="4">每日</el-radio>
-      </el-radio-group>
-      <span v-html="'\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0'"></span>
-      <el-select v-model="valueMon" placeholder="请选择">
+      <el-select v-model="selectedModelId" placeholder="请选择" @change="handleModelChange">
         <el-option
-          v-for="item in optionsMon"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
+          v-for="model in models"
+          :key="model.model_id"
+          :label="model.model_name"
+          :value="model.model_id"
+        ></el-option>
       </el-select>
-      <br>
-      <el-image>
-        <div slot="error" class="image-slot">
-          <i class="el-icon-picture-outline">图片缺失</i>
-        </div>
-      </el-image>
-      <span v-html="'\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0'"></span>
-      <el-image>
-        <div slot="error" class="image-slot">
-          <i class="el-icon-picture-outline">图片缺失</i>
-        </div>
-      </el-image>
     </div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <div>
-      <el-table
-        :data="tableData"
+    <div v-if="selectedModelId">
+      <br />
+      <div class="chart">
+        <el-card>
+          <div ref="lineChart" style="height: 400px"></div>
+        </el-card>
+      </div>
+      <br />
+      <div class="usage-list">
+        <h2>模型使用统计表</h2>
+        <ul>
+          <li v-for="(value, key) in modelUsageStats" :key="key">
+            {{ key }}: {{ value }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <el-table
+        v-loading="listLoading"
+        :data="usageList.usages"
+        element-loading-text="Loading"
         border
-        style="width: 100%">
-        <el-table-column
-          fixed
-          prop="cata"
-          label=""
-          width="180">
+        fit
+        highlight-current-row
+      >
+
+        <el-table-column align="center" label="日期"  >
+          <template v-slot="scope">
+            {{ formatDate(scope.row.use_date) }}
+          </template>
         </el-table-column>
-        <el-table-column
-          prop="jau"
-          label="1月"
-          width="180">
+        <el-table-column align="center" label="用户使用次数" >
+          <template v-slot="scope">
+            {{ scope.row.use_count }}
+          </template>
         </el-table-column>
-        <el-table-column
-          prop="feb"
-          label="2月"
-          width="180">
+        <el-table-column align="center" label="反馈错误次数" >
+          <template v-slot="scope">
+            {{ scope.row.wrong_feedback_count }}
+          </template>
         </el-table-column>
-        <el-table-column
-          prop="mar"
-          label="3月"
-          width="180">
+        <el-table-column label="反馈正确次数" align="center" >
+          <template v-slot="scope">
+            <span>{{ scope.row.right_feedback_count }}</span>
+          </template>
         </el-table-column>
-        <el-table-column
-          prop="apr"
-          label="4月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="may"
-          label="5月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="jun"
-          label="6月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="jul"
-          label="7月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="aug"
-          label="8月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="sep"
-          label="9月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="oct"
-          label="10月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="nov"
-          label="11月"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="dec"
-          label="12月"
-          width="180">
+        <el-table-column label="错误率"  align="center" >
+          <template v-slot="scope">
+            {{ (scope.row.wrong_feedback_count*100/scope.row.use_count).toFixed(2).toString()+"%"}}
+          </template>
         </el-table-column>
       </el-table>
-    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import * as echarts from 'echarts'
+
 export default {
-  data(){
-    return{
-      styleOfH1:{
-        color:'blue',
-        marginLeft:'2%'
+  data() {
+    return {
+      styleOfH1: {
+        color: 'blue',
+        marginLeft: '2%'
       },
-      styleOfChoose:{
-        marginLeft:'2%'
+      styleOfChoose: {
+        marginLeft: '2%'
       },
-      options: [{
-          label: '常用数据集',
-          options: [{
-            value: 'Minist',
-            label: '默认数据集'
-          }, {
-            value: 'Testtext1',
-            label: '测试文本1'
-          }]
-        }, {
-          label: '数据集',
-          options: [{
-            value: 'Testtext2',
-            label: '测试文本2'
-          }, {
-            value: 'Testtext3',
-            label: '测试文本3'
-          }, {
-            value: 'Testtext4',
-            label: '测试文本4'
-          }, {
-            value: 'Testtext5',
-            label: '测试文本5'
-          }]
-        }],
-        radio: 2,
-        optionsMon: [{
-          value: '选项1',
-          label: '1月'
-        }, {
-          value: '选项2',
-          label: '2月'
-        }, {
-          value: '选项3',
-          label: '3月'
-        }, {
-          value: '选项4',
-          label: '4月'
-        }, {
-          value: '选项5',
-          label: '5月'
-        },{
-          value: '选项6',
-          label: '6月'
-        },{
-          value: '选项7',
-          label: '7月'
-        },{
-          value: '选项8',
-          label: '8月'
-        },{
-          value: '选项9',
-          label: '9月'
-        },{
-          value: '选项10',
-          label: '10月'
-        },{
-          value: '选项11',
-          label: '11月'
-        },{
-          value: '选项12',
-          label: '12月'
-        }],
-        valueMon: '',
-        tableData: [{
-          cata: '用户使用次数',
-          jau:'',
-          feb:'',
-          mar:'',
-          apr:'',
-          may:'',
-          jun:'',
-          jul:'',
-          aug:'',
-          sep:'',
-          oct:'',
-          nov:'',
-          dec:''
-        }, {
-          cata: '反馈错误次数',
-          jau:'',
-          feb:'',
-          mar:'',
-          apr:'',
-          may:'',
-          jun:'',
-          jul:'',
-          aug:'',
-          sep:'',
-          oct:'',
-          nov:'',
-          dec:''
-        }, {
-          cata: '错误率',
-          jau:'',
-          feb:'',
-          mar:'',
-          apr:'',
-          may:'',
-          jun:'',
-          jul:'',
-          aug:'',
-          sep:'',
-          oct:'',
-          nov:'',
-          dec:''
-        }]
+      models: [],
+      currentModel: null,
+      selectedModelId: 0,
+      listLoading: true,
+      modelUsageData: {},
+      modelUsageStats: {},
+      usageList: [],
+      dates: [],
+      useCounts: [],
+      wrongFeedbackCounts: [],
+      rightFeedbackCounts: [],
+      lineChart: null
     }
+  },
+  methods: {
+    async fetchModels() {
+      try {
+        const response = await axios.post('http://localhost:8080/request/model')
+        this.models = response.data.models
+        this.currentModel = response.data.current_model
+        this.selectedModelId = response.data.current_model.model_id
+        this.fetchModelUsageStats()
+      } catch (error) {
+        console.error('Error fetching models:', error)
+      }
+    },
+    async fetchModelUsageStats() {
+      try {
+        const formData = new FormData()
+        formData.append('model_id', this.selectedModelId.toString())
+        const response = await axios.post('http://localhost:8080/request/usage', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        this.listLoading = false
+        this.usageList = response.data
+        this.dates = []
+        this.useCounts = []
+        this.wrongFeedbackCounts = []
+        this.rightFeedbackCounts = []
+        for (const usage of this.usageList.usages) {
+          this.dates.push(usage.use_date)
+          this.useCounts.push(usage.use_count)
+          this.wrongFeedbackCounts.push(usage.wrong_feedback_count)
+          this.rightFeedbackCounts.push(usage.right_feedback_count)
+        }
+
+        this.updateLineChart()
+      } catch (error) {
+        console.error('Error fetching model usage stats:', error)
+      }
+    },
+    async handleModelChange() {
+      if (this.selectedModelId) {
+        await this.fetchModelUsageStats()
+      }
+    },
+    formatDate(date) {
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit' }
+      return new Date(date).toLocaleDateString('zh-CN', options)
+    },
+    updateLineChart() {
+      if (!this.lineChart) {
+        this.lineChart = echarts.init(this.$refs.lineChart)
+      }
+
+      const option = {
+        title: {
+          text: '模型使用统计',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            animation: false
+          }
+        },
+        legend: {
+          data: ['用户使用次数', '反馈正确次数', '反馈错误次数'],
+          top: 30
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: this.dates,
+          axisLabel: {
+            formatter: function(value) {
+            // 在这里使用更明确的日期格式化方法
+              return echarts.format.formatTime('yyyy-MM-dd', new Date(value))
+            }
+          }
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '用户使用次数',
+            type: 'line',
+            data: this.useCounts
+          },
+          {
+            name: '反馈正确次数',
+            type: 'line',
+            data: this.rightFeedbackCounts
+          },
+          {
+            name: '反馈错误次数',
+            type: 'line',
+            data: this.wrongFeedbackCounts
+          }
+
+        ]
+      }
+      this.lineChart.setOption(option)
+    }
+  },
+  mounted() {
+    this.fetchModels()
   }
 }
 </script>
 
 <style>
-
+/* 在这里添加你的样式 */
 </style>
